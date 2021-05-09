91d65cf9142facedba30f97564d71896bdb63bc1
SadiaFirdous <74552217+SadiaFirdous@users.noreply.github.com>
2021-05-09 13:52:34 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,26 @@
+
+public class User {
+
+	String name;
+	long phoneno;
+	String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(long phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
